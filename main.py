6a283b08a4ac0156c3753fb6feaf5cb991b9bdb9
Maestro2315 <a.yashnov23@gmail.com>
2024-11-12 00:53:43 +0400

    2.0

--- main.py
+++ main.py
@@ -8,28 +8,21 @@ WEB_APP_URL = 'https://maestro2315.github.io/PC_BotTG2/'  # Замените н�
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot=bot)
 
-# Обработчик команды /start
 @dp.message_handler(commands=['start'])
 async def start_command(message: types.Message):
-    # Создаем кнопки с иконками
-    web_app_button_1 = KeyboardButton(text="💻 Процессоры", web_app=WebAppInfo(url=f"{WEB_APP_URL}?item=cpu"))
-    web_app_button_2 = KeyboardButton(text="🖥 Видеокарты", web_app=WebAppInfo(url=f"{WEB_APP_URL}?item=gpu"))
-    web_app_button_3 = KeyboardButton(text="🔧 Материнские платы", web_app=WebAppInfo(url=f"{WEB_APP_URL}?item=motherboard"))
-    web_app_button_4 = KeyboardButton(text="💾 Оперативная память", web_app=WebAppInfo(url=f"{WEB_APP_URL}?item=ram"))
-    web_app_button_5 = KeyboardButton(text="📀 Накопители", web_app=WebAppInfo(url=f"{WEB_APP_URL}?item=storage"))
+    # Создаем кнопки
+    button_cpu = KeyboardButton(text="💻 Процессоры", web_app=WebAppInfo(url=f"{WEB_APP_URL}?item=cpu"))
+    button_gpu = KeyboardButton(text="🖥 Видеокарты", web_app=WebAppInfo(url=f"{WEB_APP_URL}?item=gpu"))
+    button_motherboard = KeyboardButton(text="🔧 Материнские платы", web_app=WebAppInfo(url=f"{WEB_APP_URL}?item=motherboard"))
+    button_ram = KeyboardButton(text="💾 Оперативная память", web_app=WebAppInfo(url=f"{WEB_APP_URL}?item=ram"))
+    button_storage = KeyboardButton(text="📀 Накопители", web_app=WebAppInfo(url=f"{WEB_APP_URL}?item=storage"))
 
-    # Создаем клавиатуру в один столбец по центру
     keyboard = ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)
-    keyboard.add(web_app_button_1, web_app_button_2, web_app_button_3, web_app_button_4, web_app_button_5)
+    keyboard.add(button_cpu, button_gpu, button_motherboard, button_ram, button_storage)
 
-    # Отправляем сообщение с кнопками
-    await message.answer(
-        "Выберите комплектующие для получения подробной информации:",
-        reply_markup=keyboard
-    )
+    await message.answer("Выберите комплектующие для подробной информации:", reply_markup=keyboard)
 
 async def main():
-    # Запуск бота
     await dp.start_polling()
 
 if __name__ == '__main__':
